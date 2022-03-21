d50e2a7aa0ea8c2495bce03f9024ec8f598f7c07
vasco1m <vasco.matos.2001@gmail.com>
2022-03-21 23:42:07 +0000

    reuniao 21-03

--- TP1/main.py
+++ TP1/main.py
@@ -1,9 +1,8 @@
 # coding=utf-8
 import re
-import sys
 
 
-def json(f_nome):
+def json(dic, f_nome):
     f = open(f_nome, encoding='utf-8')
     res = open("alunos.json", "w", encoding='utf-8')
     res.write("[\n")
@@ -17,18 +16,56 @@ def json(f_nome):
                   ",\n\t\"tpc4\": " + aluno[6] + "\n}\n")
     res.write("]")
 
+
 def main():
     f = open(input())
     linha = f.readline()
-    if re.search(r'^\"?([\w/]+)\"?((\s?\,\s?\"?([\w/]+)\"?))*$',linha) != None: #Nome, Numero,Turma
-        print("0")
-    elif re.search(r'^\"?([\w/]+)\"?(\s?\,\s?\"?([\w/]+)(\{(\d+,\d+|\d+|\d+,|,\d+)\})?\"?)*$',linha) != None: # Nome,Numero,Turma{1,4}
-        print("1")
-    elif re.search(r'^\"?([\w/]+)\"?(\s?\,\s?\"?([\w/]+)(\{(\d+,\d+|\d+|\d+,|,\d+)\}\:\:(sum|media))?\"?)*$',linha) != None: #Nome,Numero,Turma{1,4}::media
-        print("2")
-    else:
-        print("-1")
-    #json("datasets/dataSetNotas.csv")
+    match = re.findall(r'\"?(([\w/]+)({(\d+,\d+|\d+|\d+,|,\d+)}(::(sum|media))?)?)\"?', linha)
+    print(match)  # APAGAR!!
+    if match:
+        lis = []
+        for linha in f:
+            dic = {}
+            line = re.findall(r'\"?([\w/ ]+)\"?', linha)
+            for i in range(0, len(match)):
+                flag = True
+                if re.match(r'^(([\w/]+)({(\d+,\d+|\d+|\d+,|,\d+)}(::(sum|media))))$', match[i][0]):
+                    # operation:
+                    sum = False
+                    if match[i][5] == "sum":
+                        sum = True
+                    s = 0
+                    split = match[i][3].split(",")
+                    num = 0
+                    for j in range(0, int(split[1])):
+                        if line[i + j].isdigit():
+                            if sum:
+                                s += int(line[i + j])
+                            num += 1
+                    if num < int(split[0]):
+                        flag = False
+                    dic[match[i][1]] = s
+                    i += num
+                elif re.match(r'^(([\w/]+)({(\d+,\d+|\d+|\d+,|,\d+)}))$', match[i][0]):
+                    ls = []
+                    split = match[i][3].split(",")
+                    num = 0
+                    for j in range(0, int(split[1])):
+                        if line[i + j].isdigit():
+                            ls.append(int(line[i + j]))
+                            num += 1
+                    if num < int(split[0]):
+                        flag = False
+                    dic[match[i][1]] = ls
+                    i += num
+                else:
+                    dic[match[i][0]] = line[i]
+            if flag:
+                lis.append(dic)
+        print(lis)  # APAGAR
+
+    # json("datasets/dataSetNotas.csv")
+
 
 if __name__ == "__main__":
     main()
