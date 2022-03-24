8c29b2a13291a583a9ada00b1311cba9e09c1670
vasco1m <vasco.matos.2001@gmail.com>
2022-03-24 15:30:12 +0000

    final sem muitas opcoes

--- TP1/main.py
+++ TP1/main.py
@@ -1,27 +1,33 @@
 # coding=utf-8
 import re
+import sys
 
 
-def json(dic, f_nome):
-    f = open(f_nome, encoding='utf-8')
-    res = open("alunos.json", "w", encoding='utf-8')
+def json(dic, name):
+    res = open(name, "w", encoding='utf-8')
     res.write("[\n")
-    f.readline()
-    for linha in f:
-        aluno = linha.split("\n")[0].split(",")
-        res.write("{\n\t\"id_aluno\": ")
-        res.write(aluno[0])
-        res.write(",\n\t\"nome\": " + aluno[1] + ",\n\t\"curso\": " + aluno[2] +
-                  ",\n\t\"tpc1\": " + aluno[3] + ",\n\t\"tpc2\": " + aluno[4] + ",\n\t\"tpc3\": " + aluno[6] +
-                  ",\n\t\"tpc4\": " + aluno[6] + "\n}\n")
+    for item in dic:
+        res.write("\t{\n\t\t")
+        for k in range(0, len(item)):
+            res.write("\"" + list(item.keys())[k] + "\": \"" + str(list(item.values())[k]) + "\"")
+            if k != len(item) - 1:
+                 res.write(",\n\t\t")
+            else:
+                res.write("\n")
+        if item != list(dic)[-1]:
+            res.write("\t},\n")
+        else:
+            res.write("\t}\n")
     res.write("]")
 
 
 def main():
-    f = open(input())
+    if len(sys.argv) < 2:
+        print("No file selected")
+        exit(0)
+    f = open(sys.argv[1])
     linha = f.readline()
     match = re.findall(r'\"?(([\w/]+)({(\d+,\d+|\d+|\d+,|,\d+)}(::(sum|media))?)?)\"?', linha)
-    print(match)  # APAGAR!!
     if match:
         lis = []
         for linha in f:
@@ -32,18 +38,22 @@ def main():
                 if re.match(r'^(([\w/]+)({(\d+,\d+|\d+|\d+,|,\d+)}(::(sum|media))))$', match[i][0]):
                     # operation:
                     sum = False
+                    media = False
                     if match[i][5] == "sum":
                         sum = True
+                    if match[i][5] == "media":
+                        media = True
                     s = 0
                     split = match[i][3].split(",")
                     num = 0
                     for j in range(0, int(split[1])):
                         if line[i + j].isdigit():
-                            if sum:
-                                s += int(line[i + j])
+                            s += int(line[i + j])
                             num += 1
                     if num < int(split[0]):
                         flag = False
+                    if media:
+                        s = s / num
                     dic[match[i][1]] = s
                     i += num
                 elif re.match(r'^(([\w/]+)({(\d+,\d+|\d+|\d+,|,\d+)}))$', match[i][0]):
@@ -62,9 +72,8 @@ def main():
                     dic[match[i][0]] = line[i]
             if flag:
                 lis.append(dic)
-        print(lis)  # APAGAR
-
-    # json("datasets/dataSetNotas.csv")
+    name = re.sub(r'(.*)\.csv', r'\1.json', f.name)
+    json(lis, name)
 
 
 if __name__ == "__main__":
